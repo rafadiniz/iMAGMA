9d6705f34435ea483aeb2b07c27d0bbc749422f2
rafadiniz <rafa.diniz.paulino@gmail.com>
2022-01-20 17:30:21 -0300

    alteração_reconstiuição

--- iMAGMA.js
+++ iMAGMA.js
@@ -20,11 +20,13 @@ let posy2 = [];
 
 let th = 0;
 
-let amp1 = 6;
+let amp2 = 0;
 
 let i;
 
 let ti = 0;
+let tx = 0;
+let ty = 0;
 let rot = 40;
 
 function preload() {
@@ -49,8 +51,8 @@ function setup() {
     posx[i] = x * 0.6;
     posy[i] = y * 0.6;
 
-    velx[i] = noise(i*0.01)*80;
-    vely[i] = noise(i*0.03)*80;
+    velx[i] = noise(i * 0.01) * 40;
+    vely[i] = noise(i * 0.03) * 40;
 
 
     posx2[i] = x * 0.6;
@@ -69,6 +71,8 @@ function draw() {
   background(240);
 
   ti += 0.01;
+  tx += 0.01;
+  ty += 0.017;
 
   //sizel+=8;
   //if (sizel >= points.getRowCount()) {
@@ -92,25 +96,32 @@ function draw() {
     posx[i] += map(noise(i * 0.09), 0, 1, -1, 1) * 3;
     posy[i] += sin(i * 0.13) * 3;
 
-    if (posx[i] < -width/2.3) {
-      posx[i] = -width/2.3 + noise(i*0.17+th) * 80;
+    if (posx[i] < -width / 2.3) {
+      posx[i] = -width / 2.3 + noise(i * 0.17 + th) * 80;
     }
-    if (posx[i] > width/2.3) {
-      posx[i] = width/2.3 + noise(i*0.14+th) * 80;
+    if (posx[i] > width / 2.3) {
+      posx[i] = width / 2.3 + noise(i * 0.14 + th) * 80;
     }
-    if (posy[i] < -height/3) {
-      posy[i] = -height/3 + sin(i*0.13+th) * 80;
+    if (posy[i] < -height / 3) {
+      posy[i] = -height / 3 + sin(i * 0.13 + th) * 80;
     }
-    if (posy[i] > height/3) {
-      posy[i] = height/3 + sin(i*0.17+th) * 80;
+    if (posy[i] > height / 3) {
+      posy[i] = height / 3 + sin(i * 0.17 + th) * 80;
     }
 
     if (mouseIsPressed) {
-      if (dist(posx[i], posy[i], mouseX-width/2, mouseY-height/2) < 20) {
-        move = true;
+      if (dist(posx[i], posy[i], mouseX - width / 2, mouseY - height / 2) < 20) {
+        amp2 -= 0.9;
+        if (amp2 <= 3) {
+          amp2 = 3;
+        }
       }
     } else {
-      move = false;
+      amp2 += 0.01;
+      if(amp2 >= 200){
+        amp2 = 200;
+      }
+
     }
 
     //if (dist(posx[i], posy[i], mouseX-width/2, mouseY-height/2) < 120) {
@@ -143,50 +154,50 @@ function draw() {
     //  }
     //}
 
-   if (mouseX > width/2-450 && mouseX < width/2+450 && mouseY > height/2-120 && mouseY < height/2+120) {
+    if (mouseX > width / 2 - 450 && mouseX < width / 2 + 450 && mouseY > height / 2 - 120 && mouseY < height / 2 + 120) {
       alpha = 255;
     } else {
       alpha = 0;
     }
 
-    if (move) {
-      //INTERPOLAÇÃO DE ARRAYS
-      //obtendo a diferença e testando
-      if (posx[i] - posx2[i] > 0) {
-        //se a diferença for maior que 0 a posição 1 decrementa
-        posx[i] -= velx[i];
-        //quando a posição 1 decresce até a posição 2, permanece nesta
-        if (posx[i] <= posx2[i]) {
-          posx[i] = posx2[i];
-        }
-      } else {
-        //se a diferença for menor que 0 a posição 1 incrementa
-        posx[i] += velx[i];
-        //quando a posição 1 alcança a posição 2, permanece nesta
-        if (posx[i] >= posx2[i]) {
-          posx[i] = posx2[i];
-        }
+
+    //INTERPOLAÇÃO DE ARRAYS
+    //obtendo a diferença e testando
+    if (posx[i] - posx2[i] > 0) {
+      //se a diferença for maior que 0 a posição 1 decrementa
+      posx[i] -= velx[i];
+      //quando a posição 1 decresce até a posição 2, permanece nesta
+      if (posx[i] <= posx2[i]) {
+        posx[i] = posx2[i];
       }
-      if (posy[i] - posy2[i] > 0) {
-        posy[i] -= vely[i];
-        if (posy[i] <= posy2[i]) {
-          posy[i] = posy2[i];
-        }
-      } else {
-        posy[i] += vely[i];
-        if (posy[i] >= posy2[i]) {
-          posy[i] = posy2[i];
-        }
+    } else {
+      //se a diferença for menor que 0 a posição 1 incrementa
+      posx[i] += velx[i];
+      //quando a posição 1 alcança a posição 2, permanece nesta
+      if (posx[i] >= posx2[i]) {
+        posx[i] = posx2[i];
+      }
+    }
+    if (posy[i] - posy2[i] > 0) {
+      posy[i] -= vely[i];
+      if (posy[i] <= posy2[i]) {
+        posy[i] = posy2[i];
+      }
+    } else {
+      posy[i] += vely[i];
+      if (posy[i] >= posy2[i]) {
+        posy[i] = posy2[i];
       }
     }
 
+
     //efeitos nos posntos
-    let cx = cos(posx[i]*0.013+ti)*6;
-    let ny = map(noise(posy[i]*0.01+ti), 0, 1, -1, 1)*6;
+    let nx = map(noise(posx[i] * 0.02 + tx), 0, 1, -1, 1) * amp2;
+    let sy = map(noise(posy[i] * 0.027 + ty), 0, 1, -1, 1) * amp2;
 
     stroke(alpha, 200);
     fill(127 + tan(posy[i] * 0.2 + ti) * 127);
-    vertex(posx[i]+cx, posy[i]+ny);
+    vertex(posx[i] + nx, posy[i] + sy);
   }
   endShape();
 }
