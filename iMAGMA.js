1638dc58a468a8cd2ec8e25d695825e4801610a3
rafadiniz <rafa.diniz.paulino@gmail.com>
2022-02-04 23:03:00 -0300

    player_solo

--- iMAGMA.js
+++ iMAGMA.js
@@ -1,8 +1,9 @@
 let img;
 let i = 0;
-let list = 794;
 
-let press = false;
+let list = 754;
+let free = true;
+
 
 function setup() {
   createCanvas(1280, 720);
@@ -12,26 +13,24 @@ function setup() {
 
 function draw() {
   //background(0);
-
-  if (press) {
-    i-=4;
-    if(i <= 30){
-      i = 30;
-    }
-  } else {
-    i+=4;
-    if (i >= list) {
-      i = list;
+  
+  //loop
+  if (free) {
+    i += 4;
+    if (i > list) {
+      free = false;
     }
   }
+  if (!free) {
+    i -= 1;
+    if (i < 600) {
+      free = true;
+    }
+  }
+
 
   loadImage('data/MAGMA_nostk/f'+ nf(i, 5) +'.png', img => {
     image(img, 0, 0, 1280, 720);
   }
   );
-}
-
-
-function mousePressed(){
-  press =! press;
 }
