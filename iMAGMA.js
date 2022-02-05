9a571faebb09fc26f08eeb9649c9eac99fc9a1d9
rafadiniz <rafa.diniz.paulino@gmail.com>
2022-02-04 22:12:10 -0300

    booleano_rebobinando

--- iMAGMA.js
+++ iMAGMA.js
@@ -1,27 +1,37 @@
 let img;
-
 let i = 0;
-
 let list = 794;
+
+let press = false;
 
 function setup() {
   createCanvas(1280, 720);
+
+  frameRate(24);
 }
 
 function draw() {
   //background(0);
 
-  if (mouseIsPressed) {
-    i = int(map(mouseX, 0, width, 0, list));
-  }else{
-    i++;
-    if(i >= list){
+  if (press) {
+    i-=4;
+    if(i <= 30){
+      i = 30;
+    }
+  } else {
+    i+=4;
+    if (i >= list) {
       i = list;
     }
   }
-  
+
   loadImage('data/MAGMA_nostk/f'+ nf(i, 5) +'.png', img => {
     image(img, 0, 0, 1280, 720);
   }
   );
+}
+
+
+function mousePressed(){
+  press =! press;
 }
