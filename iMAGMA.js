a557d826b61d3bb2bf58fafbe5ff6601423c1be0
rafadiniz <rafa.diniz.paulino@gmail.com>
2022-02-07 14:52:31 -0300

    int com o mouse (provisória)

--- iMAGMA.js
+++ iMAGMA.js
@@ -1,9 +1,8 @@
 let img;
-let i = 0;
+let i = 754;
 
 let list = 754;
 let free = true;
-
 
 function setup() {
   createCanvas(1280, 720);
@@ -13,23 +12,25 @@ function setup() {
 
 function draw() {
   //background(0);
-  
+
   //loop
-  if (free) {
-    i++;
-    if (i > list) {
-      free = false;
-    }
-  }
-  if (!free) {
-    i--;
-    if (i < 5) {
-      free = true;
-    }
-  }
+  //if (free) {
+  //  i++;
+  //  if (i > list) {
+  //    free = false;
+  //  }
+  //}
+  //if (!free) {
+  //  i--;
+  //  if (i < 5) {
+  //    free = true;
+  //  }
+  //}
 
-
-  loadImage('data/MAGMA_nostk/f'+ nf(i, 5) +'.png', img => {
+  if (mouseIsPressed) {
+    i = map(mouseX, 0, width, 0, list);
+  }
+  loadImage('data/MAGMA_nostk/f'+ nf(int(i), 5) +'.png', img => {
     image(img, 0, 0, 1280, 720);
   }
   );
